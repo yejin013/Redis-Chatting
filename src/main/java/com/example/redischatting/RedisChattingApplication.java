8e27895e544d147118efcbc5ee3851f39b160948
yejin013 <yejin013@naver.com>
2021-02-08 19:27:02 +0900

    Initial commit

---
+++ src/main/java/com/example/redischatting/RedisChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.redischatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redischatting/RedisChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redischatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
